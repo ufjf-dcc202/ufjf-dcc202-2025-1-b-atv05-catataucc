5f7258e426aaee3de27e94ec2e5a8249767e8634
catataucc <luis.campelo@estudante.ufjf.br>
2025-07-30 16:40:44 -0300

    cria estilo do tabuleiro

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const eH1 = document.querySelector("h1");
 eH1.textContent = "olá mundo!";
+
+
+const eTabuleiro = cria_tabuleiro();
+document.body.append(eTabuleiro);
+
+function cria_tabuleiro(){
+
+    const novo_tabuleiro =  document.createElement("div");
+        novo_tabuleiro.classList.add("tabuleiro");
+        novo_tabuleiro.style.background = "lighgreen";
+        novo_tabuleiro.style.minHeight = "25px";
+        return novo_tabuleiro;
+}
